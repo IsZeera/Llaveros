8aaaa05f0c7c1337e9eaef03518b84eb79d30c78
iszeera <fernandoref4@gmail.com>
2026-04-17 13:13:35 -0300

    avance5.0_dia3

---
+++ products.js
@@ -0,0 +1,64 @@
+let llaveworks = [];
+
+fetch("/data.json") .then(response => {
+    return response.json();
+}) .then(data => {
+    llaveworks = data;    
+    renderLlaveworks(llaveworks);  
+})
+
+const contenedor = document.querySelector('.sectionProducts')
+function renderLlaveworks(lista){
+
+    contenedor.innerHTML = ""; 
+
+    lista.forEach(llave => {
+        
+        const article = document.createElement('div');
+        article.classList.add('contetnPrincipal');
+
+        article.innerHTML = `
+                <div class="divImg">
+                    <img class="imgProduct" 
+                    src="${llave.image}" 
+                    alt="${llave.alt}">
+                    
+                    <a href="https://acortar.link/iSfGU3" target="_blank" rel="noreferrer"></a>
+                </div>
+                
+                <div class="descriptionContent">
+                    <h4 class="descriptionText">${llave.title}</h4>
+                    <div class="divTextPrice">
+                        <h3 class="descriptionPrice">${llave.price}</h3>
+                    </div>
+                </div>
+            </article>
+        `;
+
+        contenedor.appendChild(article);
+    });
+}
+
+
+
+
+
+
+
+
+const buscador = document.getElementById("search");
+buscador.addEventListener("input", () => {
+
+    const texto = buscador.value.toLowerCase();
+
+    const filtrados = llaveworks.filter(llave =>
+        llave.data.especifico.toLowerCase().includes(texto) ||
+        llave.title.toLowerCase().includes(texto) ||
+        llave.data.style.toLowerCase().includes(texto) 
+    );
+
+    renderLlaveworks(filtrados);
+});
+
+
+
